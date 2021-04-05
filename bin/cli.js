1ba6ee38734da81eb8c8f5c170da1ce4c7a23dc6
Chris Thoburn <runspired@gmail.com>
2021-04-05 00:09:09 -0700

    scaffold cli for our case

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,33 @@
 #!/usr/bin/env node
 'use strict';
 
-require('codemod-cli').runTransform(
-  __dirname,
-  process.argv[2] /* transform name */,
-  process.argv.slice(3) /* paths or globs */
-);
+const cli = require('codemod-cli');
+const cleanupRemovableFiles = require('../src/cleanup-files')
+
+const JS_TRANSFORM = 'rewrite-empty-component-files';
+const HBS_TRANSFORM = 'rewrite-template-for-empty-component-files';
+const TRANSFORM_ALL = 'template-only-components-codemod';
+
+const transformName = process.argv[2]; /* transform name */
+const transformArgs = process.argv.slice(3) /* paths or globs */
+
+async function runTransforms() {
+  
+    // the desired way, run these transforms together
+  if (transformName === TRANSFORM_ALL) {
+    await cli.runTransform(__dirname, TRANSFORM_JS, transformArgs);
+    await cleanupRemovableFiles();
+    await cli.runTransform(__dirname, TRANSFORM_HBS, transformArgs);
+  
+    // allow individual runs (that may fail)
+  } else if (transformName === JS_TRANSFORM || transformName === HBS_TRANSFORM) {
+    await cli.runTransform(__dirname, transformName, transformArgs);
+    if (transformName === JS_TRANSFORM) {
+      await cleanupRemovableFiles();
+    }
+  } else {
+    throw new Error(`Unknown tranform: ${transformName}`);
+  }
+}
+
+runTransforms();
---
+++ src/cleanup-files.js
@@ -0,0 +1,3 @@
+async function cleanupRemovableFiles() {}
+
+module.exports = cleanupRemovableFiles;
